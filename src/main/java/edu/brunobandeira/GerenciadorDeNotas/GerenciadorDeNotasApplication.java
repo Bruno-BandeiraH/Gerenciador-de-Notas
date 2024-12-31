88be854c4dd7189469fc865fc726aec1aa398c0a
Bruno <brunohenrique0665@gmail.com>
2024-12-30 21:45:48 -0300

    first commit

---
+++ src/main/java/edu/brunobandeira/GerenciadorDeNotas/GerenciadorDeNotasApplication.java
@@ -0,0 +1,13 @@
+package edu.brunobandeira.GerenciadorDeNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/brunobandeira/GerenciadorDeNotas/GerenciadorDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.brunobandeira.GerenciadorDeNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
