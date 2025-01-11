7d5757a569964f3f5c898d4cdf6f82c41fd64e22
Bruno <brunohenrique0665@gmail.com>
2025-01-10 23:50:59 -0300

    test: adding testFindById

--- src/test/java/edu/brunobandeira/GerenciadorDeNotas/service/ResumeServiceTest.java
+++ src/test/java/edu/brunobandeira/GerenciadorDeNotas/service/ResumeServiceTest.java
@@ -8,7 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -38,5 +40,15 @@ public class ResumeServiceTest {
         verify(resumeRepository, times(1)).save(resume);
     }
 
+    @Test
+    void testFindById() {
+        Resume resume = new Resume(1L, "Teste", "Esse é um resumo de teste unitário", LocalDateTime.now(), Arrays.asList("teste_tag1", "teste 2"));
+        when(resumeRepository.findById(1L)).thenReturn(Optional.of(resume));
 
+        Optional<Resume> found = resumeService.findById(1L);
+
+        assertTrue(found.isPresent());
+        assertEquals("Teste", found.get().getTitle());
+        verify(resumeRepository, times(1)).findById(1L);
+    }
 }
